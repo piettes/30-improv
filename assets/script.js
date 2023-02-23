c69e92dee8728d21912e334bd5736237741d3f1c
StéphanePiette <piettes2010@gmail.com>
2023-02-23 19:25:12 +0100

    add ArrowUp/ArrowDown and pause support,

--- assets/script.js
+++ assets/script.js
@@ -1,85 +1,110 @@
 jQuery(document).ready(function ($) {
 
-   var number = 1;
+    var number = 1;
     $('#numero').html(number);
 
-    $('#numero').mousedown(function(event) {
-        switch (event.which) {
-            case 1:
-                number = number + 1;
-                $('#numero').html(number);
-                break;
-            case 3:
-                number = number - 1;
-                $('#numero').html(number);
-                break;
-        };
-    });
-
-    var duration_minutes = 30;
-
-
-    function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    return {
-        'total': t,
-        'minutes': minutes,
-        'seconds': seconds
-    };
+    const incCounter = () => {
+        number = number + 1;
+        $('#numero').html(number);
     }
 
-    function initializeClock(id) {
-    
-    var clock = document.getElementById(id);
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-    var minutes = $('#minutes').val();
-    var endtime = new Date(Date.parse(new Date()) + minutes * 60 * 1000);
-    
-    window.clearInterval(window.timeinterval);
+    const decCounter = () => {
+        number = number - 1;
+        $('#numero').html(number);
+    }
+
+    $('#numero').mousedown(function (event) {
+        switch (event.which) {
+            case 1:
+                incCounter();
+                break;
+            case 3:
+                decCounter();
+                break;
+        }
+    });
+
+    let isRunning = false;
+
+    const clock = document.getElementById('clockdiv');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+    let timeLeftInSeconds;
+
     function updateClock() {
-        var t = getTimeRemaining(endtime);
-        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-        
-        if (t.total <= 0) {
-        clearInterval(timeinterval);
+        timeLeftInSeconds -= 1;
+        let minutesLeft = Math.floor(timeLeftInSeconds / 60);
+        let secondsLeft = timeLeftInSeconds % 60;
+
+        minutesSpan.innerHTML = ('0' + minutesLeft).slice(-2);
+        secondsSpan.innerHTML = ('0' + secondsLeft).slice(-2);
+
+        if (timeLeftInSeconds <= 0) {
+            clearInterval(timeinterval);
         }
     }
 
-    updateClock();
-    window.timeinterval = setInterval(updateClock, 1000);
-    
+    function initializeClock() {
+        let totalTimeInMinutes = $('#minutes').val();
+        timeLeftInSeconds = totalTimeInMinutes * 60;
+
+        clearCounter();
+        updateClock();
+        isRunning = true;
+        startCounter();
     }
 
+    const clearCounter = () => {
+        window.clearInterval(window.timeinterval);
+    }
 
-    var buttonInit = $('#init');
-    buttonInit.click(function(){
+    const startCounter = () => {
+        window.timeinterval = setInterval(updateClock, 1000);
+    }
+
+    $('#init').click(function () {
         $('#intro').show();
         $('#content').show();
         $('#intro').trigger('play');
-        $('#intro').on('ended',function(){
+        $('#intro').on('ended', function () {
             $('#intro').hide();
-            initializeClock('clockdiv');
+            initializeClock();
         });
-        
     });
-    
-    var buttonSkip = $('#skip');
-    buttonSkip.click(function(){
+
+    $('#skip').click(function () {
         $('#intro').hide();
         $('#intro').trigger('pause');
         $('#content').show();
-        initializeClock('clockdiv');        
-    });
-    
-    var buttonPause = $('#pause');
-    buttonPause.click(function(){
-        //get minutes and seconds
-        alert('Compteur en pause.');
+        initializeClock('clockdiv');
     });
 
+    $('#pause').click(function () {
+        pause();
+    });
+
+    const pause = () => {
+        console.log("pause");
+        if (isRunning) {
+            clearCounter();
+        } else {
+            startCounter();
+        }
+        isRunning = !isRunning;
+    }
+
+    document.addEventListener('keydown', function (event) {
+        console.log(event);
+
+        if (event.key === ' ') {
+            pause();
+        }
+        if (event.key === "ArrowUp") {
+            incCounter();
+        }
+        if (event.key === "ArrowDown") {
+            decCounter();
+        }
+    });
 
 });
